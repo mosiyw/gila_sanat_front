febeea3bef336937ebc4b8a5f93d7e86f86ca370
mosiyw <amirpc.a@yahoo.com>
2023-12-15 21:02:06 +0330

    category update

--- src/settings/site-settings.tsx
+++ src/settings/site-settings.tsx
@@ -26,112 +26,6 @@ export const siteSettings = {
   site_header: {
     menu: [
       {
-        id: 1,
-        path: '/',
-        label: 'menu-demos',
-        subMenu: [
-          {
-            id: 1,
-            path: '/',
-            label: 'menu-modern',
-          },
-          {
-            id: 2,
-            path: '/classic',
-            label: 'menu-classic',
-          },
-          {
-            id: 3,
-            path: '/vintage',
-            label: 'menu-vintage',
-          },
-          {
-            id: 4,
-            path: '/standard',
-            label: 'menu-standard',
-          },
-          {
-            id: 5,
-            path: '/minimal',
-            label: 'menu-minimal',
-          },
-          {
-            id: 6,
-            path: '/trendy',
-            label: 'menu-trendy',
-          },
-          {
-            id: 7,
-            path: '/elegant',
-            label: 'menu-elegant',
-          },
-          {
-            id: 8,
-            path: '/refined',
-            label: 'menu-refined',
-          },
-        ],
-      },
-      {
-        id: 2,
-        path: '/search',
-        label: 'menu-categories',
-        subMenu: [
-          {
-            id: 1,
-            path: '/search',
-            label: 'menu-fresh-vegetables',
-          },
-          {
-            id: 2,
-            path: '/search',
-            label: 'menu-diet-nutrition',
-          },
-          {
-            id: 3,
-            path: '/search',
-            label: 'menu-healthy-foods',
-          },
-          {
-            id: 4,
-            path: '/search',
-            label: 'menu-grocery-items',
-          },
-          {
-            id: 5,
-            path: '/search',
-            label: 'menu-beaf-steak',
-          },
-        ],
-      },
-      {
-        id: 3,
-        path: '/search',
-        label: 'menu-dietary',
-        subMenu: [
-          {
-            id: 1,
-            path: '/search',
-            label: 'menu-vegetarian',
-          },
-          {
-            id: 2,
-            path: '/search',
-            label: 'menu-kakogenic',
-          },
-          {
-            id: 3,
-            path: '/search',
-            label: 'menu-mediterranean',
-          },
-          {
-            id: 4,
-            path: '/search',
-            label: 'menu-organic',
-          },
-        ],
-      },
-      {
         id: 4,
         path: '/search/',
         label: 'menu-search',
@@ -204,6 +98,11 @@ export const siteSettings = {
             label: 'menu-404',
           },
         ],
+      },
+      {
+        id: 6,
+        path: '/cooperation-in-sales/',
+        label: 'menu-cooperation',
       },
     ],
     languageMenu: [
